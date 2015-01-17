a49dc23fb2a8558d9fdeab063760a8e7d7105c92
Augusto Breno <augusto.breno@trt6.jus.br>
2015-01-16 23:19:44 -0300

    Renomeando classe base para suite de teste

--- testeasy/src/test/java/org/easy/testeasy/TestEasyTestSuite.java
+++ testeasy/src/test/java/org/easy/testeasy/TestEasyTestSuite.java
@@ -1,12 +1,12 @@
 package org.easy.testeasy;
 
-import org.easy.testeasy.suite.BaseSuite;
+import org.easy.testeasy.suite.SuiteBase;
 
 /**
  * Acomoda automaticamente todos os testes do módulo common-core.
  * @author Augusto
  *
  */
-public class TestEasyTestSuite extends BaseSuite {
+public class TestEasyTestSuite extends SuiteBase {
 
 }
--- testeasy/src/test/java/org/easy/testeasy/dataloader/DataLoaderSuiteTest.java
+++ testeasy/src/test/java/org/easy/testeasy/dataloader/DataLoaderSuiteTest.java
@@ -1,12 +1,12 @@
 package org.easy.testeasy.dataloader;
 
-import org.easy.testeasy.suite.BaseSuite;
+import org.easy.testeasy.suite.SuiteBase;
 
 /**
  * Suite de testes do pacote dataloader
  * @author augusto
  *
  */
-public class DataLoaderSuiteTest extends BaseSuite{
+public class DataLoaderSuiteTest extends SuiteBase{
 
 }
